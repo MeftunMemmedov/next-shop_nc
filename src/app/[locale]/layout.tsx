f7063083311d76695e78e2c967547d7505fe7e1f
Məftun Məmmədov <mftnmusic6386@gmail.com>
2026-05-13 20:52:35 +0400

    font-files-converted-to-woff2

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -30,31 +30,32 @@ export const metadata: Metadata = {
 const quickSand = localFont({
   src: [
     {
-      path: '../fonts/quicksand/Quicksand-Light.ttf',
+      path: '../fonts/quicksand/Quicksand-Light.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../fonts/quicksand/Quicksand-Regular.ttf',
+      path: '../fonts/quicksand/Quicksand-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../fonts/quicksand/Quicksand-Medium.ttf',
+      path: '../fonts/quicksand/Quicksand-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../fonts/quicksand/Quicksand-Semibold.ttf',
+      path: '../fonts/quicksand/Quicksand-Semibold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../fonts/quicksand/Quicksand-Bold.ttf',
+      path: '../fonts/quicksand/Quicksand-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
   ],
+  display: 'swap',
 });
 
 interface RootLayoutParams {
